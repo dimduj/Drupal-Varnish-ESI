ea88d5c8da77df3282ae75ada95d4dfe6ec0c2dd
xdecock <xdecock@gmail.com>
2014-02-27 17:50:35 +0100

    Updated test.sh to account for defaults_vcl directory

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+ORIG_PWD=`pwd`
+cd defaults_vcl
 for config_vcl in *.vcl; do
-	PWD=`pwd`
-	varnishd -n /tmp -C -pvcl_dir=$PWD -f $config_vcl >/dev/null 2>&1
+	varnishd -n /tmp -C -pvcl_dir=$ORIG_PWD -f "${ORIG_PWD}/defaults_vcl/${config_vcl}" >/dev/null 2>&1
 	ERROR=$?
 	if [ $ERROR -eq 0 ] ; then
 		echo -e "\e[00;32m$config_vcl [ OK ]\e[00m";
 	else 
 		echo -e "\e[00;31m$config_vcl [ Error ] $ERROR\e[00m";
-		varnishd -n /tmp -C -pvcl_dir=$PWD -f $config_vcl;
+		varnishd -n /tmp -C -pvcl_dir=$ORIG_PWD -f $config_vcl;
 	fi;	
 done;
